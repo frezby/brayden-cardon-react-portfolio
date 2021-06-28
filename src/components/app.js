a6504b1e67d392fb2ca83704551e3bfbb312cd7f
Brayden Cardon <brayden.c547@nebo.local>
2021-06-28 09:11:27 -0600

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import moment from "moment";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Brayden Cardon Portfolio</h1>
+        <i class="fad fa-temperature-down"></i>
+
+      </div>
+    );
+  }
+}
